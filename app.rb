8675d8daafed1c0f156cd7601a7ce15fc1fecaf9
Jonathan Kupferman <jonathan@turntable.fm>
2012-07-14 23:33:14 -0400

    Added a list of supported genres.

--- app.rb
+++ app.rb
@@ -10,21 +10,30 @@ set :cache, Dalli::Client.new
 
 SOUNDCLOUD_ID = ENV['SOUNDCLOUD_ID'] || YAML.load_file("config/soundcloud.yml")["id"]
 
-PAGE_SIZE = 100
+PAGE_SIZE = 200
+
+AVAILABLE_GENRES = ["bass", "dance", "deep", "dubstep",
+                    "electronic", "house", "mashup",
+                    "progressive", "techno", "trance"]
 
 get '/' do
+  @genres = AVAILABLE_GENRES
   erb :index
 end
 
 get '/mixes/:genre' do
-  genre = params[:genre] || 'mashup'
-  key = ['soundcloud', 'tracks', genre].join('/')
+  genre = params[:genre].strip.downcase || 'mashup'
+  # make sure no one is passing in any crazy genres
+  genre = 'mashup' unless AVAILABLE_GENRES.include?(genre)
+
+  key = ['sc', 'tracks', genre].join('/')
 
   mixes = settings.cache.fetch(key) do
     client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
 
     mixes = []
-    4.times do |i|
+    5.times do |i|
+      puts "Requesting #{genre} #{PAGE_SIZE} #{i*PAGE_SIZE}"
       tracks = client.get('/tracks',
                           :genres => genre,
                           :duration => {
@@ -39,7 +48,7 @@ get '/mixes/:genre' do
       mixes.concat(tracks.to_a.reject { |t| t.nil? })
     end
 
-    mixes.sort_by { |t| t.playback_count || 0 }.reverse
+    mixes.sort_by { |t| t.playback_count || 0 }.reverse[0...100]
   end
 
   mixes[0...5].to_json
