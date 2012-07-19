ebbc0507430bf5864e46c28c93cec7965444b4c4
Jonathan Kupferman <jonathan@turntable.fm>
2012-07-19 00:13:13 -0400

    Added the ability to soft-refresh the cache.

--- bin/refresh_tracks.rb
+++ bin/refresh_tracks.rb
@@ -2,6 +2,8 @@
 require "rubygems"
 require './app'
 
+forced = ARGV && ARGV[0].to_s.downcase() == 'force'
+
 puts "Refreshing tracks"
 threads = []
 AVAILABLE_GENRES.each do |genre|
@@ -10,7 +12,7 @@ AVAILABLE_GENRES.each do |genre|
     begin
       attempts += 1
       puts "Getting #{genre} attempt #{attempts}"
-      tracks(genre, force=(attempts == 1))
+      tracks(genre, force=(forced && (attempts == 1)))
     rescue Soundcloud::ResponseError => e
       sleep 3
       puts "Response Error! #{e.inspect}"
