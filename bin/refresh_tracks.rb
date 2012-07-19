16498a592496ad677dec9df8de205a78da06ce35
Jonathan Kupferman <jonathan@turntable.fm>
2012-07-18 22:18:13 -0400

    Added threading to improve refresh_tracks performance.

--- bin/refresh_tracks.rb
+++ bin/refresh_tracks.rb
@@ -3,17 +3,22 @@ require "rubygems"
 require './app'
 
 puts "Refreshing tracks"
+threads = []
 AVAILABLE_GENRES.each do |genre|
-  attempts = 0
-  begin
-    attempts += 1
-    puts "Getting #{genre} attempt #{attempts}"
-    tracks(genre, force=true)
-  rescue Soundcloud::ResponseError => e
-    sleep 3
-    puts "Response Error! #{e.inspect}"
-    retry if attempts < 5
+  threads << Thread.new do |t|
+    attempts = 0
+    begin
+      attempts += 1
+      puts "Getting #{genre} attempt #{attempts}"
+      tracks(genre, force=(attempts == 1))
+    rescue Soundcloud::ResponseError => e
+      sleep 3
+      puts "Response Error! #{e.inspect}"
+      retry if attempts < 5
+    end
   end
 end
+
+threads.each { |t| t.join }
 
 puts "Done refreshing tracks"
