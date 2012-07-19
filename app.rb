e0a7506603444ab0eeee838cbb62ac737ba8ada0
Jonathan Kupferman <jonathan@turntable.fm>
2012-07-18 20:51:38 -0400

    Sort tracks by "freshness" algorithm. Move fetching tracks to its own method.

--- app.rb
+++ app.rb
@@ -1,60 +1,76 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'sinatra'
-require 'json'
-require 'yaml'
-require 'dalli'
-require 'soundcloud'
+require "rubygems"
+require "dalli"
+require "date"
+require "json"
+require "sinatra"
+require "soundcloud"
+require "yaml"
 
 set :cache, Dalli::Client.new
 
-SOUNDCLOUD_ID = ENV['SOUNDCLOUD_ID'] || YAML.load_file("config/soundcloud.yml")["id"]
+SOUNDCLOUD_ID = ENV["SOUNDCLOUD_ID"] || YAML.load_file("config/soundcloud.yml")["id"]
 
 FETCH_PAGE_SIZE = 200
+PAGE_FETCH_COUNT = 20
 RETURN_PAGE_SIZE = 5
 
 AVAILABLE_GENRES = ["bass", "dance", "deep", "dubstep",
                     "electronic", "house", "mashup",
                     "progressive", "techno", "trance"]
 
-get '/' do
+get "/" do
   @genres = AVAILABLE_GENRES
   erb :index
 end
 
-get '/mixes/:genre/?:page?' do
-  genre = (params[:genre] || 'mashup').strip.downcase
+get "/mixes/:genre/?:page?" do
+  genre = (params[:genre] || "mashup").strip.downcase
   # make sure no one is passing in any crazy genres
-  genre = 'mashup' unless AVAILABLE_GENRES.include?(genre)
+  genre = "mashup" unless AVAILABLE_GENRES.include?(genre)
 
   page = (params[:page] || 0).to_i
   page = 0 if page < 0 || page > 20
 
-  key = ['sc', 'tracks', genre].join('/')
-
-  mixes = settings.cache.fetch(key) do
-    client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
-
-    mixes = []
-    5.times do |i|
-      puts "Requesting #{genre} #{FETCH_PAGE_SIZE} #{i*FETCH_PAGE_SIZE}"
-      tracks = client.get('/tracks',
-                          :genres => genre,
-                          :duration => {
-                            :from => 1200000  # mixes must be a least 20 minutes
-                          },
-                          :order => 'hotness',
-                          :limit => FETCH_PAGE_SIZE,
-                          :offset => i * FETCH_PAGE_SIZE
-                          )
-
-      break if tracks.empty?
-      mixes.concat(tracks.to_a.reject { |t| t.nil? })
-    end
-
-    mixes.sort_by { |t| t.playback_count || 0 }.reverse[0...100]
-  end
+  mixes = tracks(genre)
 
   offset = page * RETURN_PAGE_SIZE
   mixes[offset...(offset + RETURN_PAGE_SIZE)].to_json
+end
+
+def tracks(genre)
+  # returns the top 100 tracks for the provided genre, sorted by freshness
+  puts "Fetching #{genre}"
+  settings.cache.fetch("topgenretracks/#{genre}") do
+    # TODO: Refresh this at a sane interval
+    client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
+
+    mixes = PAGE_FETCH_COUNT.times.map do |i|
+      puts "Requesting #{genre} #{FETCH_PAGE_SIZE} #{i*FETCH_PAGE_SIZE}"
+      params = {
+        :genres => genre,
+        :duration => {
+          :from => 1200000  # mixes must be a least 20 minutes
+        },
+        :order => "hotness",
+        :limit => FETCH_PAGE_SIZE,
+        :offset => i * FETCH_PAGE_SIZE
+      }
+      settings.cache.fetch(params.to_s) do
+        puts "Missed Cache. Fetching page #{params.to_s}"
+        tracks = client.get("/tracks", params)
+
+        tracks.to_a.reject { |t| t.nil? }
+      end
+    end
+    mixes.flatten.select { |m| m }.sort_by { |t| freshness(t) }.reverse[0...100]
+  end
+end
+
+def freshness track
+  # returns a floating point number, representing the "freshness" of the track
+  elapsed = (Date.today - Date.parse(track["created_at"]))
+  elapsed = 1 if elapsed.zero?
+  plays = track["playback_count"] || 0
+  plays.to_f / (elapsed ** 1.1)
 end
