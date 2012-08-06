5bd42b281c6144cc2b828417be043c4b6bc4acb6
Jonathan Kupferman <jonathan@turntable.fm>
2012-08-06 17:06:03 -0400

    Pull 40 pages for all genres. Make freshness algorithm more aggressive.

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ set :cache, Dalli::Client.new
 SOUNDCLOUD_ID = ENV["SOUNDCLOUD_ID"] || YAML.load_file("config/soundcloud.yml")["id"]
 
 FETCH_PAGE_SIZE = 200
-PAGE_FETCH_COUNT = 20
+PAGE_FETCH_COUNT = 40
 RETURN_PAGE_SIZE = 10
 
 AVAILABLE_GENRES = ["all", "bass", "dance", "deep", "dubstep",
@@ -69,11 +69,8 @@ def tracks(genre, force=false, page_cache={})
   settings.cache.fetch(genre_key) do
     client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
 
-    # Have all fetch significantly more since it has a lot more non-mixes
-    pages_to_fetch = genre == "all" ? PAGE_FETCH_COUNT * 2 : PAGE_FETCH_COUNT
-
     mixes = []
-    pages_to_fetch.times do |i|
+    PAGE_FETCH_COUNT.times do |i|
       puts "Requesting #{genre} #{FETCH_PAGE_SIZE} #{i*FETCH_PAGE_SIZE}"
       params = {
         :duration => {
@@ -137,5 +134,5 @@ def freshness track
   elapsed = 1 if elapsed < 1
   plays = track["playback_count"]
   plays = 1 if plays.nil? || plays < 1
-  plays.to_f / (elapsed ** 1.1)
+  plays.to_f / (elapsed ** 1.2)
 end
