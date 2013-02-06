38812ef14253ddf4941d29ca5b7beaac757cab24
Jonathan Kupferman <jonathan@turntable.fm>
2013-02-05 22:07:51 -0500

    Added a helper script to just dump the json data for a soundcloud track url

---
+++ bin/fetch_track.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require "json"
+require "soundcloud"
+require "yaml"
+
+SOUNDCLOUD_ID = ENV["SOUNDCLOUD_ID"] || YAML.load_file("./config/soundcloud.yml")["id"]
+
+client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
+track = client.get('/resolve', :url => ARGV[0])
+puts JSON.pretty_generate(track)
