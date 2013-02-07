6327f976d008e231faaabb04701b996367bb7d4b
Jonathan Kupferman <jonathan@turntable.fm>
2013-02-06 21:58:17 -0500

    Change Soundcloud searching to only search without a genre. Tracks are then split up by genre/tags.

--- bin/refresh_tracks.rb
+++ bin/refresh_tracks.rb
@@ -5,24 +5,6 @@ require './app'
 forced = ARGV && ARGV[0].to_s.downcase() == 'force'
 puts "Refreshing tracks. Forced = #{forced}"
 
-threads = []
-AVAILABLE_GENRES.each do |genre|
-  threads << Thread.new do |t|
-    attempts = 0
-    cache = {} # per-thread cache so we don't re-fetch pages when a failure occurs
-    sleep(rand() * 5) # stagger start times so we don't get 503'd right away
-    begin
-      attempts += 1
-      puts "Getting #{genre} attempt #{attempts}"
-      tracks(genre, force=(forced && (attempts == 1)), page_cache=cache)
-    rescue Soundcloud::ResponseError => e
-      sleep attempts * 2
-      puts "Response Error! #{genre} #{e.response}"
-      retry if attempts < 20
-    end
-  end
-end
-
-threads.each { |t| t.join }
+refresh_tracks()
 
 puts "Done refreshing tracks"
