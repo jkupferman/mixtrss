c8c907f02ebadcc5af56388197546262ac1f958e
Jonathan Kupferman <jonathan@turntable.fm>
2013-05-02 21:01:15 -0400

    Skip some of the hourly refreshes so we don't go over the heroku limit.

--- bin/refresh_tracks.rb
+++ bin/refresh_tracks.rb
@@ -4,6 +4,13 @@ require './app'
 
 puts "Refreshing tracks"
 
-refresh_tracks()
+# Skip 1/3 of these so we don't go over the heroku limit
+skip = ARGV[0] == 'production' && Time.now.hour % 3 > 0
+
+if skip
+  print "Skipped!"
+else
+  refresh_tracks()
+end
 
 puts "Done refreshing tracks"
