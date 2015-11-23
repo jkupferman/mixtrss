cf6fac0bc7ac707f88626f448873d885c3e2d831
Jonathan Kupferman <jmkupferman@gmail.com>
2015-11-23 14:38:36 -0500

    More blacklisterating

--- bin/fetch_track.rb
+++ bin/fetch_track.rb
@@ -9,3 +9,4 @@ SOUNDCLOUD_ID = ENV["SOUNDCLOUD_ID"] || YAML.load_file("./config/soundcloud.yml"
 client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
 track = client.get('/resolve', :url => ARGV[0])
 puts JSON.pretty_generate(track)
+puts track["user"]["id"]
