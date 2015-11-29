648e412885b8f422964e039d0bf845f8d72cecf4
Jonathan Kupferman <jmkupferman@gmail.com>
2015-11-28 22:35:47 -0500

    Finally wrote a script to more easily blacklist tracks

---
+++ bin/blacklist_track.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require "json"
+require "soundcloud"
+require "yaml"
+
+SOUNDCLOUD_ID = ENV["SOUNDCLOUD_ID"] || YAML.load_file("./config/soundcloud.yml")["id"]
+BLACKLIST_PATH = "./config/blacklist.yml"
+
+client = Soundcloud.new(:client_id => SOUNDCLOUD_ID)
+track = client.get('/resolve', :url => ARGV[0])
+user_id = track["user"]["id"].to_i
+
+if user_id && user_id > 0
+  blacklist = YAML.load_file(BLACKLIST_PATH)
+  blacklist["blacklist"]["userid"] << user_id
+  blacklist["blacklist"]["userid"].sort!
+
+  File.open(BLACKLIST_PATH, 'w') { |f| f.write(blacklist.to_yaml)  }
+end
+
